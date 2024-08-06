2d223683823817510944f1f114131abea844dc0a
bakgal0624 <bakgal0624@naver.com>
2024-08-06 18:22:28 +0900

    init.sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,7 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS `krampoline` DEFAULT CHARACTER SET utf8mb4;
 
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
-GRANT ALL ON krampoline.* TO 'root'@'localhost';
+-- GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
+-- GRANT ALL ON krampoline.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
 USE `krampoline`;
