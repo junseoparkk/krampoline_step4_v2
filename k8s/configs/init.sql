7869b0a9ca11234526ce8d80284511c80db41bd5
bakgal0624 <bakgal0624@naver.com>
2024-08-06 15:15:38 +0000

    init.sql

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,10 +1,10 @@
-CREATE SCHEMA IF NOT EXISTS `krampoline` DEFAULT CHARACTER SET utf8mb4;
+CREATE SCHEMA IF NOT EXISTS `ktbworld` DEFAULT CHARACTER SET utf8mb4;
 
 -- GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
 -- GRANT ALL ON krampoline.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 
-USE `krampoline`;
+USE `ktbworld`;
 
 CREATE TABLE `places`
 (
